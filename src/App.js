f7c050c908a09d0be812840917bb7b9e1ecc12ba
PRAVIN2506 <peterpravin2506@gmail.com>
2025-08-22 14:32:14 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
 import Homepage from './pages/Homepage';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import QuoteForm from './pages/QuoteForm';
-
 
 function App() {
   return (
-    <BrowserRouter>
-    
-    <Routes>
-
-      <Route>
-        <Route path="" element={<Homepage/>} /> 
-      <Route path="QuoteForm" element={<QuoteForm/>} /> 
-      </Route>
-
-    </Routes>
-
-
-    </BrowserRouter>
-    
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} /> 
+        <Route path="/QuoteForm" element={<QuoteForm />} /> 
+      </Routes>
+    </HashRouter>
   );
 }
 
